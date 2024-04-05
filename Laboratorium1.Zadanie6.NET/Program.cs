08c4d17689901791a27e24783f17633717367c7d
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-04-05 22:09:08 +0200

    Task 6 implementation

--- Laboratorium1.Zadanie6.NET/Program.cs
+++ Laboratorium1.Zadanie6.NET/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Laboratorium1.Zadanie6.NET
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"C:\path\to\your\source\file";
+            string destinationFilePath = @"C:\path\to\your\destination\file";
 
-Console.WriteLine("Hello, World!");
+            if (!File.Exists(sourceFilePath))
+            {
+                Console.WriteLine($"The source file '{sourceFilePath}' does not exist.");
+                return;
+            }
+
+            using FileStream sourceStream = new FileStream(sourceFilePath, FileMode.Open);
+            using FileStream destinationStream = new FileStream(destinationFilePath, FileMode.Create);
+            sourceStream.CopyTo(destinationStream);
+        }
+    }
+}
